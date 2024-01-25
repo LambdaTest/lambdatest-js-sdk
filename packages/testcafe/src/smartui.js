56f2a56687e5f548e8dd0d39483c310adfe2b4b4
Shahnawaz-Sk <shahnawaz@lambdatest.com>
2024-01-25 19:52:25 +0530

    testcafe changes

---
+++ packages/testcafe/src/smartui.js
@@ -0,0 +1,42 @@
+const utils = require('@lambdatest/sdk-utils');
+const pkgName = require('../package.json').name;
+
+async function smartuiSnapshot(t, snapshotName, options) {
+    if (!t) throw new Error("The test function's `t` argument is required.");
+    if (!snapshotName) throw new Error('The `name` argument is required.');
+    if (!(await utils.isSmartUIRunning())) throw new Error('SmartUI server is not running.');
+  
+    let log = utils.logger(pkgName);
+
+    try {
+        // Inject the DOM serialization script
+        /* eslint-disable-next-line no-new-func */
+        const resp = await utils.fetchDOMSerializer();
+
+        await t.eval(new Function(resp.body.data.dom), { boundTestRun: t });
+
+        // Serialize and capture the DOM
+        /* istanbul ignore next: no instrumenting injected code */
+        let { dom, url } = await t.eval((options) => ({
+            /* eslint-disable-next-line no-undef */
+            dom: SmartUIDOM.serialize(options),
+            url: window.location.href || document.URL,
+        }), { boundTestRun: t, dependencies: {} });
+
+        await utils.postSnapshot({
+            dom: dom,
+            url,
+            name: snapshotName,
+            options
+        }, pkgName);
+
+        log.info(`Snapshot captured: ${snapshotName}`);
+    } catch (error) {
+        // Handle errors
+        throw error;
+    }
+}
+
+module.exports = {
+    smartuiSnapshot
+}
