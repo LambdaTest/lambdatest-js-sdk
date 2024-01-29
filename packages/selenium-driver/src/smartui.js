3d41a01d07bdedd7a1663005f1f44dfae645e785
Pinank Solanki <pinanks@lambdatest.com>
2024-01-29 17:59:47 +0530

    Add warning messages

--- packages/selenium-driver/src/smartui.js
+++ packages/selenium-driver/src/smartui.js
@@ -17,8 +17,9 @@ async function smartuiSnapshot(driver, name, options = {}) {
             url: document.URL
         }), {});
 
-        await utils.postSnapshot({url, name, dom, options}, pkgName);
+        let { body } = await utils.postSnapshot({url, name, dom, options}, pkgName);
         log.info(`Snapshot captured: ${name}`);
+        if (body.data.warnings?.length !== 0) body.data.warnings.map(e => log.warn(e));
     } catch (error) {
         throw new Error(error);
     }
