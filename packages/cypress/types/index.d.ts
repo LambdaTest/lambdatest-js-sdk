8bae6968afb816a55254b9994a4eaa18b5d72992
Shahnawaz-Sk <shahnawaz@lambdatest.com>
2024-04-18 22:06:27 +0530

    TS support for cypress

---
+++ packages/cypress/types/index.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+declare global {
+    namespace Cypress {
+        interface Chainable<Subject = any> {
+            /**
+             * Captures a SmartUI snapshot.
+             * @param name The name of the snapshot.
+             * @param options Additional options for snapshot capture.
+             */
+            smartuiSnapshot(name?: string, options?: object): Chainable<Subject>;
+        }
+    }
+}
+
+export {};
