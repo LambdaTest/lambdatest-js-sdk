a286818dc1a9868ad714f4ea39fabd8b017549a7
parthkirsan <parthkirsan@lambdatest.com>
2025-10-03 14:48:15 +0530

    remove logger

--- packages/cypress/src/utils/logger.js
+++ packages/cypress/src/utils/logger.js
@@ -1,24 +1,4 @@
 const chalk = require('chalk');
 const pkgName = require('../../package.json').name;
 
-function log(level, message) {
-    if (typeof message === 'object') {
-        message = JSON.stringify(message);
-    }
-    switch (level) {
-        case 'debug':
-            message = chalk.blue(message);
-            break;
-        case 'warn':
-            message = chalk.yellow(`Warning: ${message}`);
-            break;
-        case 'error':
-            message = chalk.red(`Error: ${message}`);
-            break;
-    }
-    return `[${pkgName}] ${message}`;
-}
 
-module.exports = {
-    log
-}
